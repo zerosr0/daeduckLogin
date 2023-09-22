74115987b28ba9c05a0898e77ed3cae77845cac8
zero0 <sinter3@naver.com>
2023-09-22 17:02:10 +0900

    디자인 변경 최종 완성

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const frame = document.querySelector("#frame");
 const panel = frame.querySelectorAll(".panel li");
+const panel_m = frame.querySelectorAll(".panel-mobile li");
 
 const len = panel.length - 1;
+const len_m = panel_m.length - 1;
 const interval = 4000;
-let panelNum = 0;
+let panel_num = 0;
 let timer = null;
 
 
@@ -11,24 +13,21 @@ window.onload = () => {
   starRolling();
 };
 
-slideBtns.forEach((btn, idx) => {
-  btn.addEventListener('click', () => {
-    activation(idx);
-  });
-});
-
 function activation(index) {
   for (const el of panel) el.classList.remove('on');
   panel[index].classList.add('on');
-  panelNum = index;
+  for (const el of panel_m) el.classList.remove('on');
+  panel_m[index].classList.add('on');
+  panel_num = index;
 }
 
 function rolling() {
-  panelNum < len ? panelNum++ : (panelNum = 0);
-  activation(panelNum);
+  panel_num < len ? panel_num++ : (panel_num = 0);
+  panel_num < len_m ? panel_num++ : (panel_num = 0);
+  activation(panel_num);
 }
 
 function starRolling() {
-  activation(panelNum);
+  activation(panel_num);
   timer = setInterval(rolling, interval)
 }
